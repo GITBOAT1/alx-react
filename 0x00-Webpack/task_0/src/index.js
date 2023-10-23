34545975253d24c216d6cfe872ce1680348a3213
Joe <boat232001@yahoo.com>
2023-10-24 06:31:11 +1000

    add

--- 0x00-Webpack/task_0/src/index.js
+++ 0x00-Webpack/task_0/src/index.js
@@ -1,13 +1,18 @@
 import $ from 'jquery';
 
-$(document).ready(function() {
+// Function to add paragraphs
+function addParagraphs() {
+  const body = $('body');
   const paragraphs = [
     'Holberton Dashboard',
     'Dashboard data for the students',
-    'Copyright - Holberton School'
+    'Copyright - Holberton School',
   ];
 
-  paragraphs.forEach(function(paragraph) {
-    $('body').append('<p>' + paragraph + '</p>');
+  paragraphs.forEach((text) => {
+    body.append(`<p>${text}</p>`);
   });
-});
+}
+
+// Call the function to add paragraphs
+addParagraphs();
