e491769a777c9a8fc4ee7b68a61a64d84128de76
Joe <boat232001@yahoo.com>
2023-10-24 20:50:48 +1000

    add

--- 0x00-Webpack/task_0/src/index.js
+++ 0x00-Webpack/task_0/src/index.js
@@ -1,17 +1,8 @@
-// Import jQuery
 import $ from 'jquery';
 
-// Wait for the document to be ready
-$(document).ready(function() {
-  // Create the first paragraph element
-  const paragraph1 = $('<p>').text('Holberton Dashboard');
-
-  // Create the second paragraph element
-  const paragraph2 = $('<p>').text('Dashboard data for the students');
-
-  // Create the third paragraph element
-  const paragraph3 = $('<p>').text('Copyright - Holberton School');
-
-  // Append the paragraphs to the body of the page
-  $('body').append(paragraph1, paragraph2, paragraph3);
-});
+// Directly append paragraphs to the body
+$('body').append(`
+  <p>Holberton Dashboard</p>
+  <p>Dashboard data for the students</p>
+  <p>Copyright - Holberton School</p>
+`);
