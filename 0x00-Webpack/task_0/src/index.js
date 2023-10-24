16659c46faf2509aedf54322782c77d50cfa59f7
Joe <boat232001@yahoo.com>
2023-10-24 21:32:50 +1000

    add

--- 0x00-Webpack/task_0/src/index.js
+++ 0x00-Webpack/task_0/src/index.js
@@ -1,7 +1,8 @@
 import $ from 'jquery';
 
+$(document).ready(function() {
 // Directly append paragraphs to the body
 $('body').append(`<p>Holberton Dashboard</p>`);
 $('body').append(`<p>Dashboard data for the students</p>`);
 $('body').append(`<p>Copyright - Holberton School</p>`);
-
+});
