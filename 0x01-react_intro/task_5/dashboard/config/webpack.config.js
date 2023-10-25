b02a8fce5dada85e621508ebd30a543398487591
Joe <boat232001@yahoo.com>
2023-10-25 22:13:22 +1000

    react

--- 0x01-react_intro/task_5/dashboard/config/webpack.config.js
+++ 0x01-react_intro/task_5/dashboard/config/webpack.config.js
@@ -25,6 +25,13 @@ module.exports = {
           },
         ],
       },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
     ],
   },
   devServer: {
