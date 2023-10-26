e27b2315c427a814bbd67e04ede7a1a9558856d3
Joe <boat232001@yahoo.com>
2023-10-26 20:24:02 +1000

    t

--- 0x01-react_intro/task_5/dashboard/config/webpack.config.js
+++ 0x01-react_intro/task_5/dashboard/config/webpack.config.js
@@ -1,46 +1,44 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  entry: './src/index.js', // The entry point for your JavaScript
+  entry: './src/index.js', // Your entry point in src folder
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, '../dist'),
   },
-  devtool: 'inline-source-map', // Generate source maps for debugging
+  devServer: {
+    static: {
+      directory: path.join(__dirname, '../dist'),
+    },
+  },
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader'], // Process CSS files
-      },
-      {
-        test: /\.(png|jpe?g|gif)$/,
         use: [
-          {
-            loader: 'file-loader', // Copies images to the dist folder
-          },
-          {
-            loader: 'image-webpack-loader', // Optimize images
-          },
+          MiniCssExtractPlugin.loader,
+          'css-loader',
         ],
       },
       {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-        },
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader',
+        ],
       },
     ],
   },
-  devServer: {
-    contentBase: './dist', // Serve files from the dist directory
-    hot: true, // Enable hot module replacement
-  },
   plugins: [
+    new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      template: 'src/index.html', // Use this HTML template
+      title: '../src/index.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'styles.css',
     }),
   ],
+  devtool: 'inline-source-map',
 };
