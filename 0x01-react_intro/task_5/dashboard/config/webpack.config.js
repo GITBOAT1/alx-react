9cb82ba6642f413a188b0b939608e1f53167dc9b
Joe <boat232001@yahoo.com>
2023-10-27 07:13:22 +1000

    5'

--- 0x01-react_intro/task_5/dashboard/config/webpack.config.js
+++ 0x01-react_intro/task_5/dashboard/config/webpack.config.js
@@ -1,53 +1,57 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  mode: 'development',
-  entry: './src/index.js', // The entry point for your JavaScript
+  entry: {
+    main: path.resolve(__dirname, '../src/index.js')
+  },
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, '../dist'),
   },
-  devtool: 'inline-source-map', // Generate source maps for debugging
+  devServer: {
+    static: {
+      directory: path.join(__dirname, '../dist'),
+    },
+  },
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader'], // Process CSS files
-      },
-      {
-        test: /\.(png|jpe?g|gif)$/,
         use: [
-          {
-            loader: 'file-loader', // Copies images to the dist folder
-          },
-          {
-            loader: 'image-webpack-loader', // Optimize images
-          },
+          MiniCssExtractPlugin.loader,
+          'css-loader',
         ],
       },
       {
-        test: /\.(?:js|mjs|cjs)$/,
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader',
+        ],
+      },
+      {
+        test: /\.js$/,
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
           options: {
-            presets: [
-              ['@babel/preset-env', { targets: "defaults" }]
-            ],
-            plugins: ['@babel/plugin-proposal-class-properties']
+            presets: ['@babel/preset-env']
           }
-        },
+        }
       },
     ],
   },
-  devServer: {
-    contentBase: './dist', // Serve files from the dist directory
-    hot: true, // Enable hot module replacement
-  },
   plugins: [
+    new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      template: 'src/index.html', // Use this HTML template
+      title: 'hot reload',
+      template: './src/index.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'styles.css',
     }),
   ],
+  devtool: 'inline-source-map',
 };
